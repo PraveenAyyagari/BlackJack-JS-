fd16bc0ac4457135e8447a18b4e652e6d6e1bd53
asaipraveen10@gmail.com <asaipraveen10@gmail.com>
2022-12-25 13:38:31 -0500

    testing GIT

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-
 let firstCard = 10;
 let secondCard = 1;
 let hasBlacKJack = false;
-let isAlive =true;
+let isAlive = true;
 let message;
-let sum = firstCard + secondCard ;
-let messageEl = document.getElementById("message");
+let sum = firstCard + secondCard;
+let messageEl = document.getElementById('message');
 
-function startGame(){
-
-if(sum < 21){
-   message= "Do you want new card ? 🙂 ";
-}else if(sum === 21){
-    message=  "Wohoo!!! You hit a BlackJack ...🥳";
+function startGame() {
+  if (sum < 21) {
+    message = 'Do you want new card ? 🙂 ';
+  } else if (sum === 21) {
+    message = 'Wohoo!!! You hit a BlackJack ...🥳';
     hasBlacKJack = true;
-}else {
-    message= "You're out of the game !! 😭";
+  } else {
+    message = "You're out of the game !! 😭";
     isAlive = false;
+  }
+  message = messageEl.textContent;
 }
- message = messageEl.textContent;
-}
+
+console.log('This is Part of the testing');
