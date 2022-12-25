27c5ce77d5fa711a143059f0b9296692dc0331fd
Praveen@435 <asaipraveen10@gmail.com>
2022-12-25 00:05:45 -0500

    Added BlackJack Game related files

---
+++ index.js
@@ -0,0 +1,22 @@
+
+let firstCard = 10;
+let secondCard = 1;
+let hasBlacKJack = false;
+let isAlive =true;
+let message;
+let sum = firstCard + secondCard ;
+let messageEl = document.getElementById("message");
+
+function startGame(){
+
+if(sum < 21){
+   message= "Do you want new card ? 🙂 ";
+}else if(sum === 21){
+    message=  "Wohoo!!! You hit a BlackJack ...🥳";
+    hasBlacKJack = true;
+}else {
+    message= "You're out of the game !! 😭";
+    isAlive = false;
+}
+ message = messageEl.textContent;
+}
